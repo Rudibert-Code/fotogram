c9545bc59cd2738b276bdd0c4152ab3288b97fa7
Bjoern <onoofelogmail.com>
2026-02-27 15:30:31 +0100

    added images to JS

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let imgCollection[
-    
+let imgCollection = [
+    './img/01-bohne-closeup.jpg'
+    './img/02-fointain.jpg'
+    './img/03-hard-at-work.jpeg'
+    './img/04-some-stairs-in-tokyo.jpg'
+    './img/05-bohne-and-i.jpg'
+    './img/06-some-temple-in-japan.jpg'
 ]
 
 function renderImages(){
-
+ console.log(hi);
 }
