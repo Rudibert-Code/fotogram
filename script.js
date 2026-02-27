6b0a05c0abad9f4f9171d595160e116690e714ef
Bjoern <onoofelogmail.com>
2026-02-27 15:41:01 +0100

    JS : started work on renderImage loop

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let imgCollection = [
-    './img/01-bohne-closeup.jpg'
-    './img/02-fointain.jpg'
-    './img/03-hard-at-work.jpeg'
-    './img/04-some-stairs-in-tokyo.jpg'
-    './img/05-bohne-and-i.jpg'
+    './img/01-bohne-closeup.jpg',
+    './img/02-fointain.jpg',
+    './img/03-hard-at-work.jpeg',
+    './img/04-some-stairs-in-tokyo.jpg',
+    './img/05-bohne-and-i.jpg',
     './img/06-some-temple-in-japan.jpg'
 ]
 
 function renderImages(){
- console.log(hi);
+    console.log("works");
+    for (let index = 0; index < imgCollection.length; index++) {
+        console.log(index);
+        document.getElementById('img-location').innerHTML ('<img class="img-thumbnail" src="'imgCollection[index]'" alt="">');    
+    }
 }
