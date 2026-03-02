70f8fee0a763e4cba39458de73f0bff21842e324
Bjoern <onoofelogmail.com>
2026-03-02 19:13:09 +0100

    dialog window, added next & pre buttons + function

--- script.js
+++ script.js
@@ -8,23 +8,78 @@ let imgCollection = [
 ];
 
 const dialogWindow = document.getElementById('img-closeup');
+let savedNumber = 0;
 
-function openImageViewer(number){
+function openDialog(number){
+    savedNumber = number;
     dialogWindow.showModal();
-
     // define dialog title (h2)
     let dialogTitle = document.getElementById('dialoge-url');
     dialogTitle.innerHTML = "";
-    dialogTitle.innerHTML += imgCollection[number];
-
+    dialogTitle.innerHTML = imgCollection[number];
     // define dialog image
-    let dialogImage = document.getElementById('img-viewer');
-    dialogImage.innerHTML += `<img class="dialog-img" src="${imgCollection[number]}">`;
+    let dialogImage = document.getElementById('dialogMain');
+    dialogImage.innerHTML = `<img class="dialog-img" src="${imgCollection[number]}">`;
+    // define image number
+    let dialogImgNumber = document.getElementById('dialog-img-number');
+    dialogImgNumber.innerHTML = "";
+    number++
+    dialogImgNumber.innerHTML = number + " / " + imgCollection.length;
+}
+
+function closeDialog(){
+    dialogWindow.close();
 }
 
 function renderImages(){
     for (let i = 0; i < imgCollection.length; i++) {
         let imgBox = document.getElementById('img-location');
-        imgBox.innerHTML += `<img class="img-thumbnail" src="${imgCollection[i]}" onclick="openImageViewer('${[i]}')">`;    
+        imgBox.innerHTML += `<img class="img-thumbnail" src="${imgCollection[i]}" onclick="openDialog('${[i]}')">`; 
     }
+}
+function buttonNext(){
+    number = savedNumber;
+    i = imgCollection.length;
+    i--
+    number++
+    if (number > i) {
+        number = 0;
+    }
+    // define dialog title (h2)
+    let dialogTitle = document.getElementById('dialoge-url');
+    dialogTitle.innerHTML = "";
+    dialogTitle.innerHTML = imgCollection[number];
+    // define dialog image
+    let dialogImage = document.getElementById('dialogMain');
+    dialogImage.innerHTML = `<img class="dialog-img" src="${imgCollection[number]}">`;
+    // define image number
+    let dialogImgNumber = document.getElementById('dialog-img-number');
+    dialogImgNumber.innerHTML = "";
+    savedNumber = number;
+    number++
+    dialogImgNumber.innerHTML = number + " / " + imgCollection.length;
+    
+}
+function buttonPrevious(){
+    number = savedNumber;
+    i = imgCollection.length;
+    i--
+    number--
+    if (number < 0) {
+        number = i;
+    }
+    // define dialog title (h2)
+    let dialogTitle = document.getElementById('dialoge-url');
+    dialogTitle.innerHTML = "";
+    dialogTitle.innerHTML = imgCollection[number];
+    // define dialog image
+    let dialogImage = document.getElementById('dialogMain');
+    dialogImage.innerHTML = `<img class="dialog-img" src="${imgCollection[number]}">`;
+    // define image number
+    let dialogImgNumber = document.getElementById('dialog-img-number');
+    dialogImgNumber.innerHTML = "";
+    savedNumber = number;
+    number++
+    dialogImgNumber.innerHTML = number + " / " + imgCollection.length;
+    
 }
