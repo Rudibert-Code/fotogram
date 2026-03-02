4220cb75099c3e66015cf8a1c84a9153f86f347d
Bjoern <onoofelogmail.com>
2026-03-02 15:57:20 +0100

    added dialog window

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ let imgCollection = [
     './assets/img/06-some-temple-in-japan.jpg',
 ];
 
-const imageViewer = document.getElementById('img-viewer');
+const dialogWindow = document.getElementById('img-closeup');
 
-function openImageViewer(){
-    imageViewer.showModal();
+function openImageViewer(number){
+    dialogWindow.showModal();
+
+    // define dialog title (h2)
+    let dialogTitle = document.getElementById('dialoge-url');
+    dialogTitle.innerHTML = "";
+    dialogTitle.innerHTML += imgCollection[number];
+
+    // define dialog image
+    let dialogImage = document.getElementById('img-viewer');
+    dialogImage.innerHTML += `<img class="dialog-img" src="${imgCollection[number]}">`;
 }
 
 function renderImages(){
     for (let i = 0; i < imgCollection.length; i++) {
         let imgBox = document.getElementById('img-location');
-        imgBox.innerHTML += `<img class="img-thumbnail" src="${imgCollection[i]}" onclick="openImageViewer()">`;    
+        imgBox.innerHTML += `<img class="img-thumbnail" src="${imgCollection[i]}" onclick="openImageViewer('${[i]}')">`;    
     }
 }
